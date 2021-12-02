1a0f8c123245ed8c444ee0d4ffed5a55cce64134
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2021-12-03 00:37:13 +0100

    add Vault server installation script
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

---
+++ vagrant_scripts/vault_server_installation.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -eux
+
+apt-get update
+apt-get -y install ansible curl jq libcurl4-openssl-dev libssl-dev \
+  python3-pip sshpass
+
+curl -fsSL https://apt.releases.hashicorp.com/gpg |\
+  tee /etc/apt/trusted.gpg.d/hashicorp.asc
+
+apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+apt-get update
+apt-get -y install vault
