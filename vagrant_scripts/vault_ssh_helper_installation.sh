fd2a00402f0c83fb692a822db2e6adf0b686d4b9
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2022-01-17 16:20:45 +0100

    add sha256 verification
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- vagrant_scripts/vault_ssh_helper_installation.sh
+++ vagrant_scripts/vault_ssh_helper_installation.sh
@@ -3,12 +3,20 @@
 set -eux
 
 VERSION="0.2.1"
+SHASUM="fe26f62e5822bdf66ea4bf874d1a535ffca19af07a27ff3bcd7e344bc1af39fe"
+FILENAME="vault-ssh-helper_${VERSION}_linux_amd64.zip"
+
 VAULT_SERVER="192.168.56.40"
 
 apt-get update
 apt-get -y install unzip wget
 
-wget "https://releases.hashicorp.com/vault-ssh-helper/${VERSION}/vault-ssh-helper_${VERSION}_linux_amd64.zip"
+wget "https://releases.hashicorp.com/vault-ssh-helper/${VERSION}/${FILENAME}"
+
+if ! [ "$(sha256sum ${FILENAME} | awk '{print $1}')" = "${SHASUM}" ]; then
+  echo "Checksum mismatch. Exiting."
+  exit 1
+fi
 
 unzip -q "vault-ssh-helper_${VERSION}_linux_amd64.zip" -d /usr/local/bin
 
