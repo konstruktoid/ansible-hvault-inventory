9ed70e33ccdd388faa29ef7ee1fabeda548be2da
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2022-01-19 12:20:04 +0100

    rename vars
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- vagrant_scripts/vault_ssh_helper_installation.sh
+++ vagrant_scripts/vault_ssh_helper_installation.sh
@@ -2,23 +2,23 @@
 
 set -eux
 
-VERSION="0.2.1"
-SHASUM="fe26f62e5822bdf66ea4bf874d1a535ffca19af07a27ff3bcd7e344bc1af39fe"
-FILENAME="vault-ssh-helper_${VERSION}_linux_amd64.zip"
+VAULT_SSH_VERSION="0.2.1"
+VAULT_SSH_CHECKSUM="fe26f62e5822bdf66ea4bf874d1a535ffca19af07a27ff3bcd7e344bc1af39fe"
+VAULT_SSH_FILENAME="vault-ssh-helper_${VAULT_SSH_VERSION}_linux_amd64.zip"
 
 VAULT_SERVER="192.168.56.40"
 
 apt-get update
 apt-get -y install unzip wget
 
-wget "https://releases.hashicorp.com/vault-ssh-helper/${VERSION}/${FILENAME}"
+wget "https://releases.hashicorp.com/vault-ssh-helper/${VAULT_SSH_VERSION}/${VAULT_SSH_FILENAME}"
 
-if ! [ "$(sha256sum ${FILENAME} | awk '{print $1}')" = "${SHASUM}" ]; then
+if ! [ "$(sha256sum ${VAULT_SSH_FILENAME} | awk '{print $1}')" = "${VAULT_SSH_CHECKSUM}" ]; then
   echo "Checksum mismatch. Exiting."
   exit 1
 fi
 
-unzip -q "vault-ssh-helper_${VERSION}_linux_amd64.zip" -d /usr/local/bin
+unzip -q "vault-ssh-helper_${VAULT_SSH_VERSION}_linux_amd64.zip" -d /usr/local/bin
 
 chmod 0755 /usr/local/bin/vault-ssh-helper
 chown root:root /usr/local/bin/vault-ssh-helper
@@ -34,4 +34,4 @@ EOF
 chmod 0644 /etc/vault-ssh-helper.d/config.hcl
 chown root:root /etc/vault-ssh-helper.d/config.hcl
 
-rm -v "vault-ssh-helper_${VERSION}_linux_amd64.zip"
+rm -v "vault-ssh-helper_${VAULT_SSH_VERSION}_linux_amd64.zip"
