9bb4b1a22f10b95bd2b439f3e54542bbf41913e6
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2023-10-25 08:05:27 +0000

    convert to deb822 format
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- scripts/vault_server_installation.sh
+++ scripts/vault_server_installation.sh
@@ -11,9 +11,15 @@ apt-get --assume-yes upgrade
 apt-get --assume-yes install curl unzip
 
 curl -fsSL https://apt.releases.hashicorp.com/gpg |\
-  tee /etc/apt/trusted.gpg.d/hashicorp.asc
+  gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 
-apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+echo "Types: deb
+URIs: https://apt.releases.hashicorp.com
+Suites: $(lsb_release -cs)
+Components: main
+Architectures: $(dpkg --print-architecture)
+Signed-by: /usr/share/keyrings/hashicorp-archive-keyring.gpg" | tee /etc/apt/sources.list.d/hashicorp.sources
+
 apt-get update
 apt-get --assume-yes install vault
 
