d4775b564e9838ed9ea81cd440e9710d5aac1b97
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2023-12-04 21:44:27 +0000

    use deb822 format
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- scripts/admin_server_installation.sh
+++ scripts/admin_server_installation.sh
@@ -9,8 +9,14 @@ apt-get --assume-yes install curl jq libcurl4-openssl-dev libssl-dev \
 python3 -m pip install -U ansible hvac pip pycurl
 
 curl -fsSL https://apt.releases.hashicorp.com/gpg |\
-  tee /etc/apt/trusted.gpg.d/hashicorp.asc
+  gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 
-apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+echo "Types: deb
+URIs: https://apt.releases.hashicorp.com
+Suites: $(lsb_release -cs)
+Components: main
+Architectures: $(dpkg --print-architecture)
+Signed-by: /usr/share/keyrings/hashicorp-archive-keyring.gpg" | tee /etc/apt/sources.list.d/hashicorp.sources
+
 apt-get update
 apt-get --assume-yes install vault
